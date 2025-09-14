2af3387c0fdcc8636e1e29e323405c050aba3306
dharshriya15 <dharshriya15@gmail.com>
2025-09-14 11:27:36 +0530

    updated

--- script.py
+++ script.py
@@ -431,4 +431,4 @@ async def internal_server_error_handler(request, exc):
 import asyncio
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
